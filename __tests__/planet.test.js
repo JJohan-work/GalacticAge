7a2f6ed5d9f910c014e970dac5043586d768c3a9
Jonah Johansen <johansenjonah+git@gmail.com>
2021-02-05 11:45:14 -0800

    add test flag to constructor to set currentdate as static value for testing, rewrite testing for the static value, add mercury test - good fail

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -1,30 +1,34 @@
 import Planet from '../src/js/planet.js'
 
-describe('Planet', () => {
+describe('Planets', () => {
 
   let planet;
 
   beforeEach(() => {
-    planet = new Planet(1999,1,19);
+    planet = new Planet(1999,8,19,true);
   })
 
   test('Should create a planet object with the values of the current Year, birthday, years since birth and days since last birthday saved', () => {
-    let date = new Date(1999,1,19);
+    let date = new Date(1999,8,19);
     expect(planet.currentYear).toEqual(2021);
     expect(planet.birthday).toEqual(date);
     expect(planet.years).toEqual(21);
-    expect(planet.daysSince).toBeGreaterThanOrEqual(1);
-    expect(planet.daysSince).toBeLessThan(366)
+    expect(planet.planetAge).toEqual(0);
+    expect(planet.planetNext).toEqual(0);
+    
   });
   test('Creating the planet object with a birthday that has yet to happen this year', () => {
-    let planet2 = new Planet(1999,12,24);
-    expect(planet2.daysSince).toBeGreaterThanOrEqual(1);
-    expect(planet2.daysSince).toBeLessThan(366)
+    let planet2 = new Planet(1999,12,24,true);
+    expect(planet2.daysSince).toEqual(128);
   });
   test('Creating the planet object with a birthday that has already happened this year', () => {
-    let planet3 = new Planet(1999,0,1);
-    expect(planet3.daysSince).toBeGreaterThanOrEqual(1);
-    expect(planet3.daysSince).toBeLessThan(366)
+    let planet3 = new Planet(1999,0,1,false);
+    expect(planet3.daysSince).toEqual(35);
   });
-
+  
+  test('should set planetAge and planetNext to age on planet Mercury and next birthday on Mercury in Earth date', () => {
+    planet.mercury();
+    expect(planet.planetAge).toEqual();
+    expect(planet.planetNext).toEqual();
+  })
 });
--- src/js/planet.js
+++ src/js/planet.js
@@ -1,18 +1,24 @@
 import { parse } from "@babel/core";
 
 export default class Planet {
-  constructor(year,month,day) {
-    let n = new Date();
+  constructor(year,month,day,test=false) {
+    let n = 0;
+    if (test) {n = new Date(2021,5,2);} //sets current date as a static value for jest testing
+    else {n = new Date();} //sets current date as current date for normal operation
     this.currentYear = n.getFullYear();
     this.birthday = new Date(year,month,day);
     this.years = parseInt((n - this.birthday) * 0.00000000003154);
 
     let check = new Date(this.currentYear,month,day);
-
     let check2 = (check - n > 0 ? this.currentYear-1 : this.currentYear)
     let lastbirthday = new Date(check2,month,day);
-
     this.daysSince = parseInt((n - lastbirthday) / (1000*60*60*24));
+    this.planetAge = 0;
+    this.planetNext = 0;
+  }
+
+  mercury() {
+
   }
 
 }
