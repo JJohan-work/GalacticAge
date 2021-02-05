8f7c389f0d5397675c32f62ae0cce9fd9d7a2882
Jonah Johansen <johansenjonah+git@gmail.com>
2021-02-05 11:22:56 -0800

    add Planet constructor and modified tests to properly test

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -5,13 +5,26 @@ describe('Planet', () => {
   let planet;
 
   beforeEach(() => {
-    planet = new Planet("1999.09.19");
+    planet = new Planet(1999,1,19);
   })
 
-  test('Should create a planet object with values for age in days and age in epoch time', () => {
-    expect(planet.age).toEqual(7810);
-    expect(planet.epochAge).toEqual(937724400);
-    expect()
-  })
+  test('Should create a planet object with the values of the current Year, birthday, years since birth and days since last birthday saved', () => {
+    let date = new Date(1999,1,19);
+    expect(planet.currentYear).toEqual(2021);
+    expect(planet.birthday).toEqual(date);
+    expect(planet.years).toEqual(21);
+    expect(planet.daysSince).toBeGreaterThanOrEqual(1);
+    expect(planet.daysSince).toBeLessThan(366)
+  });
+  test('Creating the planet object with a birthday that has yet to happen this year', () => {
+    let planet2 = new Planet(1999,12,24);
+    expect(planet2.daysSince).toBeGreaterThanOrEqual(1);
+    expect(planet2.daysSince).toBeLessThan(366)
+  });
+  test('Creating the planet object with a birthday that has already happened this year', () => {
+    let planet3 = new Planet(1999,0,1);
+    expect(planet3.daysSince).toBeGreaterThanOrEqual(1);
+    expect(planet3.daysSince).toBeLessThan(366)
+  });
 
 });
--- src/js/planet.js
+++ src/js/planet.js
@@ -1,9 +1,18 @@
-export default class Planet {
-  constructor(birthDate) {
+import { parse } from "@babel/core";
 
+export default class Planet {
+  constructor(year,month,day) {
+    let n = new Date();
+    this.currentYear = n.getFullYear();
+    this.birthday = new Date(year,month,day);
+    this.years = parseInt((n - this.birthday) * 0.00000000003154);
+
+    let check = new Date(this.currentYear,month,day);
+
+    let check2 = (check - n > 0 ? this.currentYear-1 : this.currentYear)
+    let lastbirthday = new Date(check2,month,day);
+
+    this.daysSince = parseInt((n - lastbirthday) / (1000*60*60*24));
   }
 
-  // combineBlank() {
-  //   return this.input1.concat(this.input2);
-  // }
 }
